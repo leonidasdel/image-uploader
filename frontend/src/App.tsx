d642b8df99b460511d5ae78eca6748cdcc3ffdd7
leonidasdel <55215042+leonidasdel@users.noreply.github.com>
2022-04-08 16:21:44 +0300

    end styling of imageuploader component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,7 +5,7 @@ import ImageUploader from './components/ImageUploader/ImageUploader';
 function App() {
     return (
         <div className="App">
-          <ImageUploader fileTypes="image/*"/>
+          <ImageUploader fileTypes="image/*" fileTypesCaption='Your file must be an image...' fileDropZoneCaption="Drag and drop your image here" />
         </div>
     );
 }
--- frontend/src/components/ImageUploader/ImageUploader.tsx
+++ frontend/src/components/ImageUploader/ImageUploader.tsx
@@ -1,31 +1,43 @@
 import React, { useState, useEffect } from 'react';
 import './ImageUploader.scss'
+import dropzone_image from '../../assets/images/file_dropzone.svg'
 
 interface fileProps {
     fileTypes: string;
+    fileTypesCaption: string;
+    fileDropZoneCaption: string;
 }
 
-const ImageUploader = ({fileTypes}:fileProps) => {
+const ImageUploader = ({ fileTypes, fileTypesCaption, fileDropZoneCaption }: fileProps) => {
 
-    function dragOver(ev:any) {
+    function dragOver(ev: any) {
 
         ev.preventDefault();
-      
-      
-      }
-      
-      
-      function dragEnter(ev:any) {
-      
-      
-      }
+
+
+    }
+
+
+    function dragEnter(ev: any) {
+
+
+    }
 
     return (
         <div className='container'>
-            <div id="drop_zone"  onDragOver={dragOver} onDragEnter={dragEnter}  >
-                <p>Drag one or more files to this Drop Zone ...</p>
+            <h1 className='container_header'>Upload your file</h1>
+            <h2 className='container_header_sub'>{fileTypesCaption}</h2>
+            <div className="container_dropzone" onDragOver={dragOver} onDragEnter={dragEnter}  >
+                <img className="container_dropzone_image" draggable="false" src={dropzone_image} />
+                <h2 className='container_dropzone_text'>{fileDropZoneCaption}</h2>
             </div>
-
+            <h6 className='container_sub_text'>Or</h6>
+            <div className='container_upload'>
+                <label htmlFor='container_upload_input' className='container_upload_button'>
+                    Choose a file
+                </label>
+                <input id='container_upload_input' className='container_upload_input' type="file" />
+            </div>
         </div>
     );
 }
