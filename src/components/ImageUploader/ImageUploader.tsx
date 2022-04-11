c89ca1ea24b9af68c04b72ecd5978eed46b684c8
leonidasdel <55215042+leonidasdel@users.noreply.github.com>
2022-04-11 12:02:15 +0300

    refactor code to handle every step of image upload in the imageupload component

--- src/components/ImageUploader/ImageUploader.tsx
+++ src/components/ImageUploader/ImageUploader.tsx
@@ -1,15 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import './ImageUploader.scss'
 import dropzone_image from '../../assets/images/file_dropzone.svg'
+import Loading from '../Loading/Loading';
 
 interface fileProps {
     fileTypes: string;
     fileTypesCaption: string;
     fileDropZoneCaption: string;
+    step: number;
     onImageUpload: (image: any) => void;
 }
 
-const ImageUploader = ({ fileTypes, fileTypesCaption, fileDropZoneCaption, onImageUpload }: fileProps) => {
+const ImageUploader = ({ fileTypes, fileTypesCaption, fileDropZoneCaption, step, onImageUpload }: fileProps) => {
 
     const [highlight, setHighlight] = useState(false);
 
@@ -45,29 +47,38 @@ const ImageUploader = ({ fileTypes, fileTypesCaption, fileDropZoneCaption, onIma
         onImageUpload(file)
     };
 
+    const renderComponent = () => {
+        switch(step) {
+            case 2: return <Loading title='Uploading...' /> 
+            case 3:    
+            default: return (   <div className='container'>
+                                    <h1 className='container_header'>Upload your file</h1>
+                                    <h2 className='container_header_sub'>{fileTypesCaption}</h2>
+                                    <div style={{border: highlight ? 'none' : '', outline: highlight ? '4px solid  #2F80ED' : 'none'} } 
+                                        className="container_dropzone"
+                                        onDragEnter={(e) => handleEnter(e)}
+                                        onDragLeave={(e) => handleLeave(e)}
+                                        onDragOver={(e) => handleOver(e)}
+                                        onDrop={(e) => handleUpload(e)}
+                                    >
+                                        <img className="container_dropzone_image" draggable="false" src={dropzone_image} />
+                                        <h2 className='container_dropzone_text'>{fileDropZoneCaption}</h2>
+                                    </div>
+                                    <h6 className='container_sub_text'>Or</h6>
+                                    <div className='container_upload'>
+                                        <label htmlFor='container_upload_input' className='container_upload_button'>
+                                            Choose a file
+                                        </label>
+                                        <input id='container_upload_input' className='container_upload_input' type="file" />
+                                    </div>
+                                </div>
+                            )
+        }
+    }
+
 
     return (
-        <div className='container'>
-            <h1 className='container_header'>Upload your file</h1>
-            <h2 className='container_header_sub'>{fileTypesCaption}</h2>
-            <div style={{border: highlight ? 'none' : '', outline: highlight ? '4px solid  #2F80ED' : 'none'} } 
-                className="container_dropzone"
-                onDragEnter={(e) => handleEnter(e)}
-                onDragLeave={(e) => handleLeave(e)}
-                onDragOver={(e) => handleOver(e)}
-                onDrop={(e) => handleUpload(e)}
-            >
-                <img className="container_dropzone_image" draggable="false" src={dropzone_image} />
-                <h2 className='container_dropzone_text'>{fileDropZoneCaption}</h2>
-            </div>
-            <h6 className='container_sub_text'>Or</h6>
-            <div className='container_upload'>
-                <label htmlFor='container_upload_input' className='container_upload_button'>
-                    Choose a file
-                </label>
-                <input id='container_upload_input' className='container_upload_input' type="file" />
-            </div>
-        </div>
+       renderComponent()
     );
 }
 
--- src/pages/HomePage/Homepage.tsx
+++ src/pages/HomePage/Homepage.tsx
@@ -3,10 +3,10 @@ import ImageUploader from '../../components/ImageUploader/ImageUploader';
 import Loading from '../../components/Loading/Loading';
 
 const HomePage = () => {
-    const [isImageUploading, setIsImageUploading] = useState(false)
+    const [step, setStep] = useState(1)
 
     const handleUpload = async (image: any) => {
-        setIsImageUploading(true)
+        setStep(2)
         const payload = new FormData()
         payload.append('image', image);
         //logs formData beautifully
@@ -14,10 +14,7 @@ const HomePage = () => {
     };
 
     return (
-        !isImageUploading
-        ? <ImageUploader onImageUpload={(image) => handleUpload(image)} fileTypes="image/*" fileTypesCaption='Your file must be an image...' fileDropZoneCaption="Drag and drop your image here" />
-        : <Loading title="Uploading..." />
-
+        <ImageUploader step={step} onImageUpload={(image) => handleUpload(image)} fileTypes="image/*" fileTypesCaption='Your file must be an image...' fileDropZoneCaption="Drag and drop your image here" />
     )
 }
 
